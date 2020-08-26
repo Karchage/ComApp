90bbeda407c8a9884936225c486fbedb59ffa895
Karchage <glebkul2010@yandex.ru>
2020-08-27 02:27:54 +0300

    Create LoginViewModel, AdminAreaAuthorization for admin and upd Startup

--- ComApp/Startup.cs
+++ ComApp/Startup.cs
@@ -52,8 +52,16 @@ namespace ComApp
                 options.AccessDeniedPath = "/account/accessdenieed";
                 options.SlidingExpiration = true;
             });
+            //Ќастраиваем политику авторизации дл€ Admin Area
+            services.AddAuthorization(x =>
+            {
+                x.AddPolicy("AdminArea", policy => { policy.RequireRole("admin"); });
+            });
             //Mvc support
-            services.AddControllersWithViews()
+            services.AddControllersWithViews(x=>
+            {
+                x.Conventions.Add(new AdminAreaAuthorization("admin", "AdminArea"));
+            })
                 .SetCompatibilityVersion(CompatibilityVersion.Version_3_0).AddSessionStateTempDataProvider();
         }
 
@@ -73,6 +81,7 @@ namespace ComApp
 
             app.UseEndpoints(endpoints =>
             {
+                endpoints.MapControllerRoute("admin", "{area:exists}{controller=Home}/{action=Index}/{id?}");
                 endpoints.MapControllerRoute("default", "{controller=Home}/{action=Index}/{id?}");
             });
         }
