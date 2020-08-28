37637f1770449e87b5cc2988c3b8acb78c4a24e2
Karchage <glebkul2010@yandex.ru>
2020-08-28 21:19:10 +0300

    Upd Html (index.cshtml) and sass file

--- ComApp/Controllers/HomeController.cs
+++ ComApp/Controllers/HomeController.cs
@@ -2,15 +2,25 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ComApp.Domain;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ComApp.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly DataManager dataManager;
+        public HomeController(DataManager dataManager)
+        {
+            this.dataManager = dataManager;
+        }
         public IActionResult Index()
         {
-            return View();
+            return View(dataManager.TextFields.GetTextFieldByCodeWord("PageIndex"));
+        }
+        public IActionResult Contacts()
+        {
+            return View(dataManager.TextFields.GetTextFieldByCodeWord("PageContacts"));
         }
     }
 }
