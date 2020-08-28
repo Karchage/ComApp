61ad0403a2fa61cfba2d976924fb8d217cf82484
Karchage <glebkul2010@yandex.ru>
2020-08-28 22:09:00 +0300

    Create SideBarViewComponent.cs and Default.cshtml for SideBar / upd SidebarPartial.cshtml

---
+++ ComApp/Models/ViewComponents/SideBarViewComponent.cs
@@ -0,0 +1,23 @@
+﻿using ComApp.Domain;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComApp.Models.ViewComponents
+{
+    public class SideBarViewComponent : ViewComponent
+    {
+        private readonly DataManager dataManager;
+        public SideBarViewComponent(DataManager dataManager)
+        {
+            this.dataManager = dataManager;
+        }
+        public Task<IViewComponentResult> InvokeAsync()
+        {
+            return Task.FromResult((IViewComponentResult)View("Default", dataManager.ServiceItems.GetServiceItems()));
+        }
+    }
+}
